77a6b6c3646b139539593564ec043ad5b5380c5c
Hamza Miskini <miskini.hamza@nure.ua>
2021-05-09 13:08:17 +0200

    configured nodemon to run typescript without compiling to JS

--- source/controllers/sample.ts
+++ source/controllers/sample.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 import logging from '../config/logging';
 
-const NAMESPACE = 'This is quick Test!!';
+const NAMESPACE = 'Server is running!!';
 const pathController = (req: Request, res: Response, next: NextFunction) => {
-    logging.info(NAMESPACE, `Sample health check route called`);
+    logging.info(NAMESPACE, `PathController route called`);
 
     return res.status(200).json({
         path: '/'
@@ -11,7 +11,7 @@ const pathController = (req: Request, res: Response, next: NextFunction) => {
 };
 
 const secondPathController = (req: Request, res: Response, next: NextFunction) => {
-    logging.info(NAMESPACE, `Sample health check route called`);
+    logging.info(NAMESPACE, `secondPathController route called`);
 
     return res.status(200).json({
         path: `/${req.params.path}`
@@ -19,7 +19,7 @@ const secondPathController = (req: Request, res: Response, next: NextFunction) =
 };
 
 const thirdPathController = (req: Request, res: Response, next: NextFunction) => {
-    logging.info(NAMESPACE, `Sample health check route called`);
+    logging.info(NAMESPACE, `thirdPathController route called`);
 
     return res.status(200).json({
         path: `/${req.params.path}/${req.params.secondPath}`
